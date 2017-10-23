898efef426e5c12efa87672c0384eb60d3d9ed27
PhazzaT <piodul@op.pl>
2017-10-23 21:57:03 +0200

    Changes (for worse, IMO)

--- src/processing.rs
+++ src/processing.rs
@@ -10,30 +10,14 @@ type PixelRegion<'a> = &'a mut [[u8; 3]];
 type Histogram = [usize; 32768]; // Histogram for RGB565 colors
 type Color = [u8; 3];
 
-struct Bucket<'a> {
+#[derive(Clone)]
+struct Bucket {
     volume: usize,
+    weight: usize,
     bounds: [Range<usize>; 3],
-    pixels: PixelRegion<'a>,
 }
 
 // Utility
-
-// Partitions by given predicate. Elements which evaluate
-// to false are put before those which evaluate to true.
-// Returns the size of the "false" range.
-fn partition_by<'a, T, F>(arr: &'a mut [T], f: F) -> usize
-        where F: Fn(&T) -> bool {
-    let mut begin = 0;
-    let mut end = 0;
-    while end < arr.len() {
-        if !f(&arr[end]) {
-            arr.swap(begin, end);
-            begin += 1;
-        }
-        end += 1;
-    }
-    begin
-}
 
 // Color functions
 
@@ -42,6 +26,11 @@ fn to_rgb555(color: Color) -> Color {
     let g = color[1] >> 3;
     let b = color[2] >> 3;
     [r, g, b]
+}
+
+fn to_rgb888(color: Color) -> Color {
+    let promote = |x| (x << 3) | (x >> 2);
+    [promote(color[0]), promote(color[1]), promote(color[2])]
 }
 
 fn pack_rgb555(color: Color) -> u16 {
@@ -60,8 +49,21 @@ fn generate_histogram<'a>(pixels: PixelRegion<'a>) -> Box<Histogram> {
 
 // Bucket-related functions
 
-fn make_bucket<'a>(pixels: PixelRegion<'a>)
-        -> Bucket<'a> {
+fn bucket_weight(bounds: &[Range<usize>; 3], histogram: &Histogram) -> usize {
+    let mut sum = 0;
+    for i in bounds[0].clone() {
+        for j in bounds[1].clone() {
+            for k in bounds[2].clone() {
+                let p = pack_rgb555([i as u8, j as u8, k as u8]);
+                sum += histogram[p as usize];
+            }
+        }
+    }
+    sum
+}
+
+fn make_bucket_from_pixels<'a>(pixels: PixelRegion<'a>)
+        -> Bucket {
     let bounds = {
         let mut bounds = [0..0, 0..0, 0..0];
         for i in 0..3 {
@@ -74,15 +76,28 @@ fn make_bucket<'a>(pixels: PixelRegion<'a>)
     let volume = bounds.iter().map(|d| d.len()).product();
     Bucket {
         volume: volume,
+        weight: pixels.len(),
         bounds: bounds,
-        pixels: pixels,
     }
 }
 
-fn split_bucket<'a>(mut bucket: Bucket<'a>, histogram: &'a Histogram)
-        -> (Option<Bucket<'a>>, Option<Bucket<'a>>) {
-    if bucket.pixels.len() == 1 {
-        return (Some(bucket), None);
+fn split_bucket_along(bucket: Bucket, axis: usize, split_position: usize,
+        histogram: &Histogram) -> (Bucket, Bucket) {
+    let mut b0 = bucket;
+    let mut b1 = b0.clone();
+
+    b0.bounds[axis].end = split_position;
+    b0.weight = bucket_weight(&b0.bounds, histogram);
+    b1.bounds[axis].start = split_position;
+    b1.weight = bucket_weight(&b1.bounds, histogram);
+
+    (b0, b1)
+}
+
+fn split_bucket<'a>(bucket: Bucket, histogram: &'a Histogram)
+        -> (Bucket, Option<Bucket>) {
+    if bucket.volume == 1 {
+        return (bucket, None);
     }
 
     // Find the widest axis, and the traversal order
@@ -113,6 +128,7 @@ fn split_bucket<'a>(mut bucket: Bucket<'a>, histogram: &'a Histogram)
         }
         buckets[i] = sum;
     }
+    let pixel_sum = buckets.iter().sum();
 
     // Figure out how to partition buckets, so as to minimize
     // new buckets' size difference
@@ -121,49 +137,38 @@ fn split_bucket<'a>(mut bucket: Bucket<'a>, histogram: &'a Histogram)
         prefix_sums[i] = prefix_sums[i - 1] + buckets[i - 1];
     }
     let best_split = (0..1 << 5).into_iter().min_by_key(|i| {
-        (2 * prefix_sums[*i] as i64 - bucket.pixels.len() as i64).abs()
+        (2 * prefix_sums[*i] as i64 - pixel_sum as i64).abs()
     }).unwrap();
 
-    // println!("{:?}", prefix_sums);
-    // println!("best_split: {}", best_split);
-    // println!("bucket.bounds: {:?}", bucket.bounds);
-    // println!("buckets: {:?}", buckets);
-    // println!("prefix_sum: {:?}", prefix_sums);
-    // println!("{:?}", (0..1 << 5).into_iter().map(|i| {
-    //     (2 * prefix_sums[i] as i64 - bucket.pixels.len() as i64).abs()
-    // }).collect::<Vec<_>>());
-    // println!("bucket sum: {}", buckets.iter().sum::<usize>());
-    // println!("bucket.pixels.len(): {}", bucket.pixels.len());
-    // assert!(buckets.iter().sum::<usize>() == bucket.pixels.len());
-
     if prefix_sums[best_split] == 0
-            || prefix_sums[best_split] == bucket.pixels.len() {
+            || prefix_sums[best_split] == pixel_sum {
         // Can't split into two non-empty buckets
-        return (Some(bucket), None);
+        return (bucket, None);
     }
 
     // Partition into two subranges
-    let split_position = partition_by(&mut bucket.pixels, |c| { 
-        (c[x] >> 3) < best_split as u8
-    });
-    let (low, high) = bucket.pixels.split_at_mut(split_position);
-    // println!("split_position: {}", split_position);
-
-    ( Some(make_bucket(low))
-    , Some(make_bucket(high)) )
+    let (low, high) = split_bucket_along(bucket, x, best_split, histogram);
+    (low, Some(high))
 }
 
-fn color_from_bucket<'a>(bucket: Bucket<'a>) -> Color {
-    let mut ret = [0, 0, 0];
-    for c in bucket.pixels.iter() {
-        for i in 0..3 {
-            ret[i] += c[i] as u64;
+fn color_from_bucket(bucket: Bucket, histogram: &Histogram) -> Color {
+    let mut avg = [0, 0, 0];
+    for i in bucket.bounds[0].clone() {
+        for j in bucket.bounds[1].clone() {
+            for k in bucket.bounds[2].clone() {
+                let p = pack_rgb555([i as u8, j as u8, k as u8]);
+                let mult = histogram[p as usize];
+                avg[0] += i * mult;
+                avg[1] += j * mult;
+                avg[2] += k * mult;
+            }
         }
     }
-    for i in 0..3 {
-        ret[i] /= bucket.pixels.len() as u64;
-    }
-    [ret[0] as u8, ret[1] as u8, ret[2] as u8]
+
+    avg[0] /= bucket.weight;
+    avg[1] /= bucket.weight;
+    avg[2] /= bucket.weight;
+    to_rgb888([avg[0] as u8, avg[1] as u8, avg[2] as u8])
 }
 
 pub fn quantize<'a>(pixels: PixelRegion<'a>, max_color_count: usize)
@@ -172,7 +177,7 @@ pub fn quantize<'a>(pixels: PixelRegion<'a>, max_color_count: usize)
     assert!(max_color_count >= 2 && max_color_count <= 256);
 
     let histogram = generate_histogram(pixels);
-    let mut queue = vec![make_bucket(pixels)];
+    let mut queue = vec![make_bucket_from_pixels(pixels)];
 
     {
         let mut iter = |f: fn(&Bucket) -> usize, target: usize| {
@@ -183,22 +188,18 @@ pub fn quantize<'a>(pixels: PixelRegion<'a>, max_color_count: usize)
                 }
                 queue.sort_unstable_by_key(f);
                 let b = queue.pop().unwrap();
-                if b.pixels.len() == 0 {
-                    queue.push(b);
-                    continue;
-                }
-                n_color -= 1;
                 let (b0, b1) = split_bucket(b, &histogram);
-                for b in b0.into_iter().chain(b1.into_iter()) {
+                queue.push(b0);
+                b1.map(|b| {
                     queue.push(b);
                     n_color += 1;
-                }
+                });
             }
         };
         // iter(|b| b.pixels.len(), max_color_count);
-        iter(|b| b.pixels.len(), max_color_count * 3 / 4);
-        iter(|b| b.pixels.len() * b.volume, max_color_count);
+        iter(|b| b.weight, max_color_count * 3 / 4);
+        iter(|b| b.weight * b.volume, max_color_count);
     }
 
-    queue.into_iter().map(color_from_bucket).collect()
+    queue.into_iter().map(|b| color_from_bucket(b, &histogram)).collect()
 }
